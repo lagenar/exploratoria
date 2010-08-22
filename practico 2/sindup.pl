2f4eb56143c44f6b3183d722881ea599b2a2d041
Lucas Moauro <lagenar@gmail.com>
2010-08-22 15:23:00 -0700

    delete antes que la llamada recursiva

--- practico 2/sindup.pl
+++ practico 2/sindup.pl
@@ -3,6 +3,6 @@ delete(X, [X|Ys], Zs) :- delete(X, Ys, Zs).
 delete(X, [Y|Ys], [Y|Zs]) :- X \= Y, delete(X, Ys, Zs).
 
 sin_duplicados([], []).
-sin_duplicados([X|Xs], [X|Zs]) :- sin_duplicados(Xs, Ys), delete(X, Ys, Zs).
+sin_duplicados([X|Xs], [X|Zs]) :-  delete(X, Ys, Zs), sin_duplicados(Xs, Ys).
 
 	
